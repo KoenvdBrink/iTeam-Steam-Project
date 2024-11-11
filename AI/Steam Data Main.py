72adf89d1b6a374053a6ecbea98445a98dbb8149
Koen <koen.vandenbrink@student.hu.nl>
2024-11-11 16:18:21 +0100

    first commit. steam API requests. basic steam user data function.

---
+++ AI/Steam Data Main.py
@@ -0,0 +1,64 @@
+import requests
+import datetime
+from datetime import datetime
+
+api_key = "5409DBECBF319D8375208A2EC86A66FE"
+steamid = "76561198030044972"
+
+
+
+def get_player_summ(steam_id):
+    """
+    Returns basic profile information for a list of 64-bit Steam IDs.
+    https://developer.valvesoftware.com/wiki/Steam_Web_API#GetGlobalAchievementPercentagesForApp_.28v0001.29
+    for detailed information about returned dictionary contents.
+    :param steam_id: int
+    :return: Dict
+    """
+    url = f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/'
+    params = {
+        'key':api_key,
+        'steamids':steam_id
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+def get_owned_games(steam_id):
+    """
+
+    :param steam_id:
+    :return:
+    """
+    url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/'
+    params = {
+        'key': api_key,
+        'steamid': steam_id,
+        'format': 'json'
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+def last_logged_off(steam_id):
+    """
+    Gives the date the user last logged off on steam.
+    :param steam_id: int
+    :return: str
+    """
+    time_unix = get_player_summ(steam_id)['response']['players'][0]['lastlogoff']
+    return datetime.fromtimestamp(time_unix)
+
+
+def offline_for(steam_id):
+    """
+    calculates the amount of hours it has been since the user last logged off on steam.
+    rounds down to the nearest full hour.
+    returns -1 of the user is online
+    :param steam_id: int
+    :return: float/int
+    """
+    now = datetime.now()
+    then = last_logged_off(steam_id)
+    if get_player_summ(steam_id)['response']['players'][0]['personastate'] != 0:
+        return -1
+    else:
+        return abs(divmod((now - then).total_seconds(),3600)[0])
