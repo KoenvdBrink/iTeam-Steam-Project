443ed7caa89c3309c9bbc85b6493b5162a4b680e
nizarmak <nizar.makhloufi@student.hu.nl>
2025-01-11 19:44:20 +0100

    Removed temporary code for database testing. (Might revert)

--- AI/Fetch_data_server.py
+++ AI/Fetch_data_server.py
@@ -13,41 +13,41 @@ def fetch_average_playtime():
     try:
         # Gebruik mock-waarde
         print("[INFO] Gebruik van gecachede waarde omdat database niet draait.")
-        return 90.0  # Gemiddelde speeltijd in uren als voorbeeld
+        # return 90.0  # Gemiddelde speeltijd in uren als voorbeeld voor testen
         # Establish connection
-        # conn = psycopg2.connect(
-        #     host=host,
-        #     port=port,
-        #     database=database,
-        #     user=pguser,
-        #     password=pgpass
-        # )
-        # playtime = 0
-        # games = 0
+        conn = psycopg2.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=pguser,
+            password=pgpass
+        )
+        playtime = 0
+        games = 0
 
-        # # Create a cursor to interact with the database
-        # cursor = conn.cursor()
+        # Create a cursor to interact with the database
+        cursor = conn.cursor()
 
-        # # Execute a query
-        # cursor.execute("SELECT average_playtime_hrs FROM app_details;")
+        # Execute a query
+        cursor.execute("SELECT average_playtime_hrs FROM app_details;")
 
-        # # Fetch all rows from the executed query
-        # rows = cursor.fetchall()
+        # Fetch all rows from the executed query
+        rows = cursor.fetchall()
 
 
-        # for row in rows:
-        #     playtime += row[0]
-        #     games += 1
+        for row in rows:
+            playtime += row[0]
+            games += 1
 
     except psycopg2.Error as e:
         print(f"Error connecting to PostgreSQL database: {e}")
 
     finally:
         # Close the cursor and connection
-        # if cursor:
-        #     cursor.close()
-        # if conn:
-        #     conn.close()
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
 
-        pass
-    # return playtime/games
+        # pass voor testen
+    return playtime/games
